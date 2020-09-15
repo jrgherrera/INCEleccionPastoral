cd204a939ec2e4c9192e1430a2670ef4b6fdd212
Jorge Herrera <jrgherrera@gmail.com>
2020-09-14 21:37:25 -0600

    Fix search after lastname change

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -30,7 +30,7 @@ function clearList () {
 function renderList (users, searchVal = '') {
     for (let i = 0; i < users.length; i++) {
         const userData = users[i];
-        if (userData.name.toLowerCase().includes(searchVal)) {
+        if (`${userData.lastName.toLowerCase()} ${userData.name.toLowerCase()}`.includes(searchVal)) {
             const $user = $userTemplate.cloneNode(true);
             $user.id = `user-${userData.id}`;
             if (userData.hasVoted) {
